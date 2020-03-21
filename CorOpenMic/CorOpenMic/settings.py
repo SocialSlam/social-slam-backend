eaa23d6291ff8a72bfbbe384bec433da5941d0f7
Marc_The_Shark <marc.piper@blindside.pro>
2020-03-21 10:42:31 +0100

    Created initial models for users app

--- CorOpenMic/CorOpenMic/settings.py
+++ CorOpenMic/CorOpenMic/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+AUTH_USER_MODEL = 'users.SlamUser'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users',
+    'slams'
 ]
 
 MIDDLEWARE = [
--- CorOpenMic/users/models.py
+++ CorOpenMic/users/models.py
@@ -1,4 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
+class Skill(models.Model):
+
+    skill_category_choices = [
+        (0, 'Music'),
+        (1, 'Dance'),
+        (2, 'Poetry'),
+        (3, 'Art'),
+        (4, 'Fitness'),
+        (5, 'Pitch'),
+    ]
+
+    skill_category = models.SmallIntegerField(choices=skill_category_choices)
+
+
+class UserSkillLevel(models.Model):
+
+    skill_level_choices = [
+        (0, 'Beginner'),
+        (1, 'Novice'),
+        (2, 'Intermediate'),
+        (3, 'Expert'),
+        (4, 'Professional'),
+    ]
+
+    skill_level = models.SmallIntegerField(choices=skill_level_choices)
+
+
+class SlamUser(AbstractUser):
+
+    skills = models.ManyToManyField(Skill, through=UserSkillLevel)
+    
