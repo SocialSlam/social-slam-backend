e0f50ed5e92fab0ce37d6ce1de5bd0d085eedf7c
Marc_The_Shark <marc.piper@blindside.pro>
2020-04-22 19:58:46 +0200

    Added missing JSONWebTokenBackend

--- social_slam_api/schema/schema.py
+++ social_slam_api/schema/schema.py
@@ -52,6 +52,7 @@ class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
+
     register = Register.Field()
 
 
