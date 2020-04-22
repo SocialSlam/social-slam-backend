4e4513f56a78d1d58eba8e77f35e9e29e13bd117
Marc_The_Shark <marc.piper@blindside.pro>
2020-04-22 20:39:01 +0200

    Updated to superior auth module

--- social_slam_api/schema/schema.py
+++ social_slam_api/schema/schema.py
@@ -1,5 +1,6 @@
 import graphene
-import graphql_jwt
+from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
 from graphene_django import DjangoObjectType
 
 from users.models import Slammer, Skill, UserSkillLevel
@@ -20,7 +21,7 @@ class UserSkillLevelType(DjangoObjectType):
         model = UserSkillLevel
 
 
-class Query(graphene.ObjectType):
+class Query(UserQuery, MeQuery, graphene.ObjectType):
     all_slammers = graphene.List(UserType)
     all_skills = graphene.List(SkillType)
 
@@ -31,29 +32,26 @@ class Query(graphene.ObjectType):
         return Skill.objects.all()
 
 
-class Register(graphene.Mutation):
-    class Arguments:
-        first_name = graphene.String(required=True)
-        last_name = graphene.String()
-        email = graphene.String(required=True)
-        password = graphene.String()
-        username = graphene.String(required=True)
-    user = graphene.Field(UserType)
-
-    def mutate(self, info, **kwargs):
-        password = kwargs.pop('password')
-        user = Slammer.objects.create(**kwargs)
-        user.set_password(password)
-        user.save()
-        return Register(user=user)
-
-
 class Mutation(graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
+    register = mutations.Register.Field()
+    verify_account = mutations.VerifyAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
+    password_change = mutations.PasswordChange.Field()
+    update_account = mutations.UpdateAccount.Field()
+    archive_account = mutations.ArchiveAccount.Field()
+    delete_account = mutations.DeleteAccount.Field()
+    send_secondary_email_activation = mutations.SendSecondaryEmailActivation.Field()
+    verify_secondary_email = mutations.VerifySecondaryEmail.Field()
+    swap_emails = mutations.SwapEmails.Field()
+    remove_secondary_email = mutations.RemoveSecondaryEmail.Field()
 
-    register = Register.Field()
+    # django-graphql-jwt inheritances
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    verify_token = mutations.VerifyToken.Field()
+    refresh_token = mutations.RefreshToken.Field()
+    revoke_token = mutations.RevokeToken.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- social_slam_api/settings/settings.py
+++ social_slam_api/settings/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    "graphql_auth",
+    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
     'rest_framework',
+    'django_filters',
     'users',
     'slams',
     'address'
@@ -57,15 +60,34 @@ MIDDLEWARE = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'graphql_jwt.backends.JSONWebTokenBackend',
+    "graphql_auth.backends.GraphQLAuthBackend",
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 GRAPHENE = {
     'SCHEMA': 'schema.schema.schema',
     'MIDDLEWARE': [
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
     ],
+}
+
+GRAPHQL_JWT = {
+    "JWT_ALLOW_ANY_CLASSES": [
+        "graphql_auth.mutations.Register",
+        "graphql_auth.mutations.VerifyAccount",
+        "graphql_auth.mutations.ResendActivationEmail",
+        "graphql_auth.mutations.SendPasswordResetEmail",
+        "graphql_auth.mutations.PasswordReset",
+        "graphql_auth.mutations.ObtainJSONWebToken",
+        "graphql_auth.mutations.VerifyToken",
+        "graphql_auth.mutations.RefreshToken",
+        "graphql_auth.mutations.RevokeToken",
+        "graphql_auth.mutations.VerifySecondaryEmail",
+    ],
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
 }
 
 ROOT_URLCONF = 'settings.urls'
