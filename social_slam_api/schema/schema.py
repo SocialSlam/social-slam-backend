376ff7b2b7a97db256e8752d34f85f758ac80267
Marc_The_Shark <marc.piper@blindside.pro>
2020-04-22 15:30:30 +0200

    Maded m2m work with improper naming

--- social_slam_api/schema/schema.py
+++ social_slam_api/schema/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from users.models import Slammer
+from users.models import Slammer, Skill, UserSkillLevel
 
 
 class UserType(DjangoObjectType):
@@ -9,11 +9,25 @@ class UserType(DjangoObjectType):
         model = Slammer
 
 
+class SkillType(DjangoObjectType):
+    class Meta:
+        model = Skill
+
+
+class UserSkillLevelType(DjangoObjectType):
+    class Meta:
+        model = UserSkillLevel
+
+
 class Query(graphene.ObjectType):
     all_slammers = graphene.List(UserType)
+    all_skills = graphene.List(SkillType)
 
     def resolve_all_slammers(self, info, **kwargs):
         return Slammer.objects.all()
+
+    def resolve_all_skills(self, info, **kwargs):
+        return Skill.objects.all()
 
 
 schema = graphene.Schema(query=Query)
--- social_slam_api/slams/admin.py
+++ social_slam_api/slams/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from users.models import Slammer
+from users.models import Slammer, Skill, UserSkillLevel
 
 admin.site.register(Slammer)
+admin.site.register(Skill)
+admin.site.register(UserSkillLevel)
