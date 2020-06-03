9490391c1e4bea13f723fa41568bfe8b9b3850a3
Marc_The_Shark <marc.piper@blindside.pro>
2020-06-04 01:11:44 +0200

    Added AbstractTitle model. Added documentation

--- social_slam_api/utils/models.py
+++ social_slam_api/utils/models.py
@@ -1,16 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class AbstractTimestamp(models.Model):
-
+    """
+    Abstract Base Model that handles recording temporal data.
+    """
     class Meta:
         abstract = True
 
-    created_at = models.DateTimeField()
-    last_updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
 
 
-class AbstractLedger(AbstractTimestamp):
+class AbstractTitle(models.Model):
+    """
+    Abstract Base Model that holds a title and a description
+    """
+    class Meta:
+        abstract = True
 
     title = models.TextField(max_length=256, null=True)
     description = models.TextField(max_length=2048, null=True)
+
+
+class AbstractLedger(AbstractTimestamp):
+    """
+    Abstract Base Model that records User information with respect to creating, modifying, and deleting an object.
+    """
+    class Meta:
+        abstract = True
+
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True,
+                                   related_name='%(class)s_created')
+    last_modified_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,
+                                         related_name='%(class)s_modifications')
