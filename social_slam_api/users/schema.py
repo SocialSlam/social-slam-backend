919218cf0c174eac521803e68087c590747eae56
Marc_The_Shark <marc.piper@blindside.pro>
2020-06-10 19:03:14 +0200

    removed unnecessary users query

--- social_slam_api/users/schema.py
+++ social_slam_api/users/schema.py
@@ -6,12 +6,6 @@ from graphql_auth.schema import UserQuery, MeQuery
 from users.models import Skill, UserSkillLevel
 
 
-class UserType(DjangoObjectType):
-    class Meta:
-        model = get_user_model()
-        filter_fields = ['id']
-        interfaces = (graphene.relay.Node,)
-
 class SkillType(DjangoObjectType):
     class Meta:
         model = Skill
@@ -25,11 +19,7 @@ class UserSkillLevelType(DjangoObjectType):
 
 
 class UserQuery(UserQuery, MeQuery, graphene.ObjectType):
-    users = DjangoConnectionField(UserType)
     skills = DjangoConnectionField(SkillType)
-
-    def resolve_users(self, info, **kwargs):
-        return get_user_model().objects.all()
 
     def resolve_skills(self, info, **kwargs):
         return Skill.objects.all()
